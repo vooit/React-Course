b2f22115ad7b994d0e26fe62e89d9430e1d692b5
vooit <wojciechpac@gmail.com>
2017-08-21 00:34:08 +0200

    put all into arrow fn

--- src/index.js
+++ src/index.js
@@ -3,21 +3,26 @@ import ReactDOM from 'react-dom';
 
 import events from './data/events.json';
 
+const getLatestItems = () => {
+    return (
+        <ul>
+            {
+                events.map(item => {
+                    const date = new Date(item.date);
+                    if (date >= Date.now()) {
+                        return (
+                            <li key={item.id}>
+                                <strong>{item.name}</strong>
+                                <br/>
+                                Gdzie: {item.place} <br/>
+                                Kiedy: {item.date} - {item.time}
+                            </li>)
+                    }
+                })
+            }
+        </ul>
+    );
+}
 
-ReactDOM.render(
-    <ul>
-        {
-            events.map(item => {
-                const date = new Date(item.date);
-                if (date >= Date.now()) {
-                    return (
-                        <li key={item.id}>
-                            <strong>{item.name}</strong>
-                            <br/>
-                            Gdzie: {item.place} <br/>
-                            Kiedy: {item.date} - {item.time}
-                        </li>)
-                }
-            })
-        }
-    </ul>, document.getElementById('root'));
+
+ReactDOM.render(getLatestItems(), document.getElementById('root'));
